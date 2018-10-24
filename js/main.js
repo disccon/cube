9085dff245093a0f6fa8ef6f76c5ac34864042f2
Disccon <dissccon@gmail.com>
2018-10-25 02:06:56 +0300

    es6 fist Commit

--- js/main.js
+++ js/main.js
@@ -1,134 +1,128 @@
 window.onload = function () {
     const tableRef = document.getElementsByClassName('table')[0];
-
+    const tableRefRows = tableRef.rows;
     const plusBottom = document.getElementsByClassName('button-plus_bottom')[0];
-    plusBottom.addEventListener("click", AddRow);
-    function AddRow() {
-        let row = tableRef.insertRow(-1);
-        if(tableRef.rows[0].cells.length !== 0){
-            let colCount = tableRef.rows[0].cells.length;
-            for (let i = 0; i < colCount; i++) {
-                row.insertCell(-1);
-            }
-        }
-        if(tableRef.rows[0].cells.length === 0){
-            let colCount = 0;
-            for (let i = 0; i <= colCount; i++) {
-                row.insertCell(-1);
-            }
-        }
-    };
-
     const plusRight = document.getElementsByClassName('button-plus_right')[0];
-    plusRight.addEventListener("click",AddCell);
-    function AddCell() {
-        if(tableRef.rows.length !== 0) {
-            let trArr = tableRef.getElementsByTagName("tr");
-            for (let i = 0, l = trArr.length; i < l; i++) {
-                trArr[i].insertCell(-1);
-            }
-        }
-        if(tableRef.rows.length === 0 ){
-            let row = tableRef.insertRow(-1);
-            let colCount = 0;
-            for (let i = 0; i <= colCount; i++) {
-                row.insertCell(-1);
-            }
-        }
+    const minusLeft = document.getElementsByClassName('button-minus_left')[0];
+    const minusTop = document.getElementsByClassName('button-minus_top')[0];
+    let TableMouseleave;
+    const wrapperTable = document.getElementsByClassName('wrapper-table')[0];
+
+    let targetIndexRow = -1;
+    let targetIndexCell = -1;
+
+
+    let tableborderSpacing = tableRef.style.borderSpacing
+    let heightCell =  tableRefRows[0].cells[0].getBoundingClientRect().height + 2;
+    let widthCell =  tableRefRows[0].cells[0].getBoundingClientRect().width + 2;
+
+    plusBottom.addEventListener("click", AddRow);
+    function AddRow(){
+        let row = tableRef.insertRow(-1);
+        for (let value of tableRefRows[0].cells) {
+            row.insertCell();
+        };
+
     };
 
-    const minusLeft = document.getElementsByClassName('button-minus_left')[0];
+    plusRight.addEventListener("click",AddCell);
+    function AddCell(){
+        for (let index of tableRefRows) {
+            index.insertCell();
+        };
+    };
+
+
     minusLeft.addEventListener("click",deleteRow);
     function deleteRow(){
         let minusLeftPositionTop = parseInt(minusLeft.style.top, 10);
 
-        if ((minusLeftPositionTop + 52) > tableRef.getBoundingClientRect().height && tableRef.rows.length > 2) {
-            minusLeft.style.top = minusLeftPositionTop - 52 + 'px';
+
+        if ((minusLeftPositionTop + heightCell) > tableRef.getBoundingClientRect().height && tableRefRows.length > 2) {
+            minusLeft.style.top = minusLeftPositionTop - heightCell + 'px';
         }
 
-        if(tableRef.rows.length > 1) {
-            let IndexRow = (minusLeftPositionTop - 5) / 52;
-            tableRef.deleteRow(IndexRow)
+        if(tableRefRows.length > 1) {
+            if(targetIndexRow >= tableRefRows.length){
+                targetIndexRow = targetIndexRow -1;
+            }
+            tableRef.deleteRow(targetIndexRow)
         }
-        if(tableRef.rows.length === 1) {
-            setTimeout(function() {
-                minusLeft.classList.remove('button-minus_animation-opacity');
-                setTimeout(function() {
+        if(tableRefRows.length === 1) {
+            minusLeft.classList.remove('button-minus_animation-opacity');
+            setTimeout( () => {
                     minusLeft.classList.remove('button-minus_animation-display');
-                }, 100);
-            }, 10);
+            }, 300);
         }
     }
 
-    const minusTop = document.getElementsByClassName('button-minus_top')[0];
+
     minusTop.addEventListener("click",deleteCells);
     function deleteCells() {
         let minusTopPositionLeft = parseInt( minusTop.style.left, 10);
-        const IndexSells = (minusTopPositionLeft - 5) / 52;
 
-        if ((minusTopPositionLeft + 52) > tableRef.getBoundingClientRect().width && tableRef.rows[0].cells.length > 2) {
-            minusTop.style.left = minusTopPositionLeft - 52 + 'px';
+        if ((minusTopPositionLeft + widthCell) > tableRef.getBoundingClientRect().width && tableRefRows[0].cells.length > 2) {
+            minusTop.style.left = minusTopPositionLeft - widthCell + 'px';
         }
 
-        let trArr = tableRef.rows;
-        if(tableRef.rows[0].cells.length > 1) {
-            for (let i = 0; i < trArr.length; i++) {
-                trArr[i].deleteCell(IndexSells)
+        if(tableRefRows[0].cells.length > 1) {
+            if(targetIndexCell >= tableRefRows[0].cells.length){
+                targetIndexCell = targetIndexCell -1;
             }
+            for (let index of tableRefRows) {
+                index.deleteCell(targetIndexCell)
+            };
         }
 
-        if(tableRef.rows[0].cells.length === 1) {
-            setTimeout(function() {
-                minusTop.classList.remove('button-minus_animation-opacity');
-                setTimeout(function() {
-                    minusTop.classList.remove('button-minus_animation-display');
-                }, 100);
-            }, 10);
+        if(tableRefRows[0].cells.length === 1) {
+             minusTop.classList.remove('button-minus_animation-opacity');
+             setTimeout( () => {
+                 minusTop.classList.remove('button-minus_animation-display');
+             }, 300);
         }
     }
 
-
-
-    let TableMouseleave;
-    const wrapperTable = document.getElementsByClassName('wrapper-table')[0];
 
     wrapperTable.addEventListener("mouseover",mouseoverTable);
     function mouseoverTable(event) {
         let target = event.target;
         clearTimeout(TableMouseleave)
-        if(tableRef.rows[0].cells.length > 1) {
+        if(tableRefRows[0].cells.length > 1) {
             minusTop.classList.add('button-minus_animation-display');
-            setTimeout(function() {
+            setTimeout(() => {
                 minusTop.classList.add('button-minus_animation-opacity');
             }, 10);
         }
 
-        if(tableRef.rows.length > 1) {
+        if (target.tagName === "TD") {
+            targetIndexRow = target.parentElement.rowIndex;
+            targetIndexCell = target.cellIndex;
+            minusLeft.style.top = (target.parentNode.rowIndex * heightCell)+ 5 + "px";
+            minusTop.style.left = (target.cellIndex * widthCell)+ 5 + "px";
+            // minusLeft.dataset.row = target.parentElement.rowIndex;
+            // minusTop.dataset.cell = target.cellIndex;
+        }
+
+        if(tableRefRows.length > 1) {
             minusLeft.classList.add('button-minus_animation-display');
-            setTimeout(function() {
+            setTimeout(() => {
                 minusLeft.classList.add('button-minus_animation-opacity');
             }, 10);
         }
-
-        setTimeout(function() {
-            minusLeft.classList.add('button-minus_animation-opacity');
-        }, 10);
-            minusLeft.style.top = (target.parentNode.rowIndex * 52)+ 5 + "px";
-            minusTop.style.left = (target.cellIndex * 52)+ 5 + "px";
 
     }
 
 
     wrapperTable.addEventListener("mouseleave",mouseleaveTable);
     function mouseleaveTable(){
-        TableMouseleave = setTimeout(function() {
+        TableMouseleave = setTimeout(() => {
             minusLeft.classList.remove('button-minus_animation-opacity');
             minusTop.classList.remove('button-minus_animation-opacity');
-            setTimeout(function() {
+            setTimeout(() => {
                 minusLeft.classList.remove('button-minus_animation-display');
                 minusTop.classList.remove('button-minus_animation-display');
-            }, 200);
-        }, 900);
+            }, 3300);
+        }, 3000);
 
 
     }
