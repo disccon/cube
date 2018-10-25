8f1fe3b913fdc80c20223bfede4f325f9050805c
Disccon <dissccon@gmail.com>
2018-10-25 22:14:36 +0300

    es6 2 Commit

--- js/main.js
+++ js/main.js
@@ -8,128 +8,114 @@ window.onload = function () {
     let TableMouseleave;
     const wrapperTable = document.getElementsByClassName('wrapper-table')[0];
 
-    let targetIndexRow = -1;
-    let targetIndexCell = -1;
+    let targetIndexRow = null;
+    let targetIndexCell = null;
+
+    let heightCell = tableRefRows[0].cells[0].getBoundingClientRect().height + 2;
+    let widthCell = tableRefRows[0].cells[0].getBoundingClientRect().width + 2;
 
 
-    let tableborderSpacing = tableRef.style.borderSpacing
-    let heightCell =  tableRefRows[0].cells[0].getBoundingClientRect().height + 2;
-    let widthCell =  tableRefRows[0].cells[0].getBoundingClientRect().width + 2;
-
-    plusBottom.addEventListener("click", AddRow);
-    function AddRow(){
-        let row = tableRef.insertRow(-1);
-        for (let value of tableRefRows[0].cells) {
-            row.insertCell();
-        };
-
-    };
-
-    plusRight.addEventListener("click",AddCell);
-    function AddCell(){
-        for (let index of tableRefRows) {
-            index.insertCell();
-        };
-    };
-
-
-    minusLeft.addEventListener("click",deleteRow);
-    function deleteRow(){
-        let minusLeftPositionTop = parseInt(minusLeft.style.top, 10);
-
-
-        if ((minusLeftPositionTop + heightCell) > tableRef.getBoundingClientRect().height && tableRefRows.length > 2) {
-            minusLeft.style.top = minusLeftPositionTop - heightCell + 'px';
-        }
-
-        if(tableRefRows.length > 1) {
-            if(targetIndexRow >= tableRefRows.length){
-                targetIndexRow = targetIndexRow -1;
+    class Table {
+        addRow() {
+            let row = tableRef.insertRow(-1);
+            for (let value of tableRefRows[0].cells) {
+                row.insertCell();
             }
-            tableRef.deleteRow(targetIndexRow)
-        }
-        if(tableRefRows.length === 1) {
-            minusLeft.classList.remove('button-minus_animation-opacity');
-            setTimeout( () => {
-                    minusLeft.classList.remove('button-minus_animation-display');
-            }, 300);
-        }
-    }
-
-
-    minusTop.addEventListener("click",deleteCells);
-    function deleteCells() {
-        let minusTopPositionLeft = parseInt( minusTop.style.left, 10);
-
-        if ((minusTopPositionLeft + widthCell) > tableRef.getBoundingClientRect().width && tableRefRows[0].cells.length > 2) {
-            minusTop.style.left = minusTopPositionLeft - widthCell + 'px';
-        }
-
-        if(tableRefRows[0].cells.length > 1) {
-            if(targetIndexCell >= tableRefRows[0].cells.length){
-                targetIndexCell = targetIndexCell -1;
-            }
+        };
+        addCell() {
             for (let index of tableRefRows) {
-                index.deleteCell(targetIndexCell)
-            };
+                index.insertCell();
+            }
+        };
+        deleteRow() {
+            let minusLeftPositionTop = parseInt(minusLeft.style.top, 10);
+            if ((minusLeftPositionTop + heightCell) > tableRef.getBoundingClientRect().height && tableRefRows.length > 2) {
+                minusLeft.style.top = minusLeftPositionTop - heightCell + 'px';
+
+            }
+            if (tableRefRows.length > 1) {
+                tableRef.deleteRow(targetIndexRow >= tableRefRows.length ? targetIndexRow = targetIndexRow - 1 : targetIndexRow)
+            }
+            if (tableRefRows.length === 1) {
+                minusLeft.classList.remove('button-minus_animation-opacity');
+                setTimeout(() => {
+                    minusLeft.classList.remove('button-minus_animation-display');
+                }, 300);
+            }
         }
 
-        if(tableRefRows[0].cells.length === 1) {
-             minusTop.classList.remove('button-minus_animation-opacity');
-             setTimeout( () => {
-                 minusTop.classList.remove('button-minus_animation-display');
-             }, 300);
+        deleteCells() {
+            let minusTopPositionLeft = parseInt(minusTop.style.left, 10);
+            if ((minusTopPositionLeft + widthCell) > tableRef.getBoundingClientRect().width && tableRefRows[0].cells.length > 2) {
+                minusTop.style.left = minusTopPositionLeft - widthCell + 'px';
+            }
+            if (tableRefRows[0].cells.length > 1) {
+                if (targetIndexCell >= tableRefRows[0].cells.length) {
+                    targetIndexCell = targetIndexCell - 1;
+                }
+                for (let index of tableRefRows) {
+                    index.deleteCell(targetIndexCell)
+                }
+                ;
+            }
+            if (tableRefRows[0].cells.length === 1) {
+                minusTop.classList.remove('button-minus_animation-opacity');
+                setTimeout(() => {
+                    minusTop.classList.remove('button-minus_animation-display');
+                }, 300);
+            }
+        }
+
+        mouseoverTable(event) {
+            let target = event.target;
+            clearTimeout(TableMouseleave)
+            if (tableRefRows[0].cells.length > 1) {
+                minusTop.classList.add('button-minus_animation-display');
+                setTimeout(() => {
+                    minusTop.classList.add('button-minus_animation-opacity');
+                }, 10);
+            }
+            if (target.tagName === "TD") {
+                targetIndexRow = target.parentElement.rowIndex;
+                targetIndexCell = target.cellIndex;
+                minusLeft.style.top = (target.parentNode.rowIndex * heightCell) + 5 + "px";
+                minusTop.style.left = (target.cellIndex * widthCell) + 5 + "px";
+            }
+            if (tableRefRows.length > 1) {
+                minusLeft.classList.add('button-minus_animation-display');
+                setTimeout(() => {
+                    minusLeft.classList.add('button-minus_animation-opacity');
+                }, 10);
+            }
+        }
+
+        mouseleaveTable() {
+            TableMouseleave = setTimeout(() => {
+                minusLeft.classList.remove('button-minus_animation-opacity');
+                minusTop.classList.remove('button-minus_animation-opacity');
+                setTimeout(() => {
+                    minusLeft.classList.remove('button-minus_animation-display');
+                    minusTop.classList.remove('button-minus_animation-display');
+                }, 3300);
+            }, 3000);
         }
     }
 
+    let tableClass = new Table();
 
-    wrapperTable.addEventListener("mouseover",mouseoverTable);
-    function mouseoverTable(event) {
-        let target = event.target;
-        clearTimeout(TableMouseleave)
-        if(tableRefRows[0].cells.length > 1) {
-            minusTop.classList.add('button-minus_animation-display');
-            setTimeout(() => {
-                minusTop.classList.add('button-minus_animation-opacity');
-            }, 10);
-        }
+    plusBottom.addEventListener("click", tableClass.addRow);
 
-        if (target.tagName === "TD") {
-            targetIndexRow = target.parentElement.rowIndex;
-            targetIndexCell = target.cellIndex;
-            minusLeft.style.top = (target.parentNode.rowIndex * heightCell)+ 5 + "px";
-            minusTop.style.left = (target.cellIndex * widthCell)+ 5 + "px";
-            // minusLeft.dataset.row = target.parentElement.rowIndex;
-            // minusTop.dataset.cell = target.cellIndex;
-        }
+    plusRight.addEventListener("click", tableClass.addCell);
 
-        if(tableRefRows.length > 1) {
-            minusLeft.classList.add('button-minus_animation-display');
-            setTimeout(() => {
-                minusLeft.classList.add('button-minus_animation-opacity');
-            }, 10);
-        }
+    minusLeft.addEventListener("click", tableClass.deleteRow);
 
-    }
+    minusTop.addEventListener("click", tableClass.deleteCells);
 
+    wrapperTable.addEventListener("mouseover", tableClass.mouseoverTable);
 
-    wrapperTable.addEventListener("mouseleave",mouseleaveTable);
-    function mouseleaveTable(){
-        TableMouseleave = setTimeout(() => {
-            minusLeft.classList.remove('button-minus_animation-opacity');
-            minusTop.classList.remove('button-minus_animation-opacity');
-            setTimeout(() => {
-                minusLeft.classList.remove('button-minus_animation-display');
-                minusTop.classList.remove('button-minus_animation-display');
-            }, 3300);
-        }, 3000);
+    wrapperTable.addEventListener("mouseleave", tableClass.mouseleaveTable);
 
-
-    }
 }
-
-
-
 
 
 
